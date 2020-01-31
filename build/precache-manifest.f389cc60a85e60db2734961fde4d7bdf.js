e77b701c48c6228f92b554fddc7315d5cbe63c3b
Ayush Raj <ayushrajmathur@gmail.com>
2020-01-31 19:15:23 +0530

    build added

---
+++ build/precache-manifest.f389cc60a85e60db2734961fde4d7bdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0642b8735f1ba50e70589719b6e67d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30c9bc33235ca6eff967",
+    "url": "/static/css/main.34948f3c.chunk.css"
+  },
+  {
+    "revision": "026ddd50f5e8b05a17de",
+    "url": "/static/js/2.5dfa96cd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.5dfa96cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30c9bc33235ca6eff967",
+    "url": "/static/js/main.0145ee4e.chunk.js"
+  },
+  {
+    "revision": "193e98dc51d9e13c8350",
+    "url": "/static/js/runtime-main.b23607f7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
